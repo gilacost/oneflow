ec179e09dc9a9713677a2394941b36629d69c049
gilacost <josepgiraltdlacoste@gmail.com>
2017-06-26 22:41:46 +0200

    fosa 2

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule OneflowEx.Mixfile do
 
   defp package do
     [
-      files: ["lib"],
+      files: ["lib/models/*"],
       maintainers: ["Josep Lluis Giralt D'Lacoste"],
       licenses: ["MIT License"],
       links: %{"GitHub" => "https://github.com/gilacost/oneflow_ex"}
